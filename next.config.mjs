2124478024f3bfd67b417750256e7f9c0bb52e47
Maia Salti <maiasalti@Maias-MacBook-Pro.local>
2025-09-13 12:23:41 -0400

    add third post with interactive chart

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import nextMDX from '@next/mdx';
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const withMDX = nextMDX({
+  extension: /\.mdx?$/, // allow .md and .mdx files
+});
+
+const nextConfig = {
+  pageExtensions: ['js', 'jsx', 'md', 'mdx'], // tell Next.js to treat these as pages
+};
+
+export default withMDX(nextConfig);
+
