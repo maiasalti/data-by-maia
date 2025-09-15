bf46e0964b6ecc20a058a5c04089200a6a28bc3a
Maia Salti <maiasalti@Maias-MacBook-Pro.local>
2025-09-14 20:59:06 -0400

    analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import Link from 'next/link';
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -37,6 +38,7 @@ export default function RootLayout({ children }) {
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <main className="min-h-screen">
           {children}
+          <Analytics />
         </main>
         <footer className="bg-gray-800 text-white py-4 text-center">
           <div className="max-w-6xl mx-auto px-4">
